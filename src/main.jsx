9e75570b706bc5bd246d5933fc748860f20ff3ef
nsadeh <nnnsadeh@gmail.com>
2023-12-04 18:57:44 -0500

    working

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import './index.css'
+
+const options = [
+  { id: '1', title: 'Option 1' },
+  { id: '2', title: 'Option 2' },
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App options={options} onSelect={() => {}} label="Test Multidrop" />
   </React.StrictMode>
 )
